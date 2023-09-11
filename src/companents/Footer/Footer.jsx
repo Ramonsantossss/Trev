aa7a16b742c44070710b9ef49e19445be0891f40
Ramonsantossss <103340294+Ramonsantossss@users.noreply.github.com>
2023-09-11 13:00:52 +0000

    Footer novo

--- src/companents/Footer/Footer.jsx
+++ src/companents/Footer/Footer.jsx
@@ -4,66 +4,17 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className="Footer">
-      <div className="footer_top">
-        <div className="fotter_top_blog1">
-          <div className="footer_first">
-            <div className="f_first_top">
-              <div className="logo_f">
-                <Link to={"/"}>
-                  
-                  <span>Mangás Online.</span>
-                </Link>
-                <p className="logo_bottm_text"></p>
-              </div>
-
-            </div>
-            <div className="f_secend_bottom">
-              <p>Mangas online todos os direitos reservados</p>
-              <p>Atenção: Este site não hospeda nada em seu servidor.<br />
-                Todo conteudo é provido por terceiros e não afiliados.</p>
-            </div>
-          </div>
-          <div className="footer_secend">
-            <ul className="ul_f ul_f_fist">
-              <span>
-              <li>
-                <Link to={"/"}>Home</Link>
-                <i></i>
-              </li>
-              <li>
-                <Link to={"/"}>News</Link>
-                <i></i>
-              </li>
-              <li>
-                <Link to={"/"}>Categories</Link>
-                <i></i>
-              </li>
-              </span>
-            </ul>
-            <ul className="ul_f ul_f_secend">
-              <span>
-                <li>
-                  <Link to={"/"}>New Epsodes</Link>
-                  <i></i>
-                </li>
-                <li>
-                  <Link to={"/"}>Highlights</Link>
-                  <i></i>
-                </li>
-              </span>
-
-            </ul>
-          </div>
+    <div className="Foer">
+        <footer>
+        <div class="footer-buttons">
+            <button class="round-button"><i class="fas fa-star"></i></button>
+            <button class="round-button"><i class="fas fa-heart"></i></button>
+            <Link to="/" class="home-button round-button"><i class="fas fa-home"></i></Link>
+            <button class="round-button"><i class="fas fa-envelope"></i></button>
+            <button class="round-button"><i class="fas fa-cog"></i></button>
         </div>
-        
-      </div>
-      <div className="footer_bottom_f">
-        <div className="fT_line"></div>
-        <p>
-          Copyright DMCA (English) Termos de Uso Politica de Privacidade todos os direitos reservados 2021
-        </p>
-      </div>
+    </footer>
+      
     </div>
   );
 }
